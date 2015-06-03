78cfa54534b4f938b6708b6b07c647eb2e812c5e
Alexander Mireles <alexandermireles0@gmail.com>
2015-06-02 21:29:18 -0700

    authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,12 +7,14 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
-	Def create
+	def create
 	@user = User.new(user_params)
-	if @user.save
-		redirect_to users_path
-	else
-		render :new
+		if @user.save
+			session[:user_id] = @user.id.to_s
+			redirect_to users_path
+		else
+			render :new
+		end
 	end
 end
 
@@ -20,5 +22,5 @@ private
 
 def user_params
 	params.require(:user).permit(:name, :email, :password, :password_confirmation)
-	end
 end
+
