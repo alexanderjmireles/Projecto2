13bfe6637e6fa0d3e7d65508ba21ced450751db9
Alexander Mireles <alexandermireles0@gmail.com>
2015-06-05 12:48:23 -0700

    applying code for searchbar

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,10 @@
 class MembersController < ApplicationController
   def index
+  	if params[:search]
+  		@members = Member.search(params[:search]).order("created_at DESC")
+  	else
+  		@members = Member.order("created_at DESC")
+  	end
   end
 
   def new
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,11 @@
 class Member < ActiveRecord::Base
+	attr_accessible :title, :content
+
+	validates :title, presence: true, uniqueness: true
+	validates :content, presence: true
+
+	def self.search(query)
+
+		where("title like ?",  "%#{query}%")
+	end
 end
