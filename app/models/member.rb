080632ba51edb6b40bbb37fa04e0ae44e53d32ff
Alexander Mireles <alexandermireles0@gmail.com>
2015-06-05 20:31:37 -0700

    removed search bar

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,11 +1,2 @@
 class Member < ActiveRecord::Base
-	attr_accessible :title, :content
-
-	validates :title, presence: true, uniqueness: true
-	validates :content, presence: true
-
-	def self.search(query)
-
-		where("title like ?",  "%#{query}%")
 	end
-end
